5c25f2556ca25345636c60fc44a279183c1d28ba
luisrodriguez <codekommissar@gmail.com>
2017-11-21 20:47:41 -0400

    Hide API keys

---
+++ bot.js
@@ -0,0 +1,5 @@
+var Twit = require("twit");
+var moment = require('moment');
+var config = require("./config");
+
+var T = new Twit(config);
--- config.js
+++
@@ -1,7 +0,0 @@
-// Add your personal info for the bot to work in here
-module.exports = {
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...'
-}
