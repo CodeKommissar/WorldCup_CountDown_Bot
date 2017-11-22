93d7ca87300362c4a80382751eaefdac9f3fe360
luisrodriguez <codekommissar@gmail.com>
2017-11-21 20:34:47 -0400

    Added initial configuration, but without any real info

---
+++ config.js
@@ -0,0 +1,7 @@
+// Add your personal info for the bot to work in here
+module.exports = {
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...'
+}
