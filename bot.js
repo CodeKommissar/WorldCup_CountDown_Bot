73a723e49fa407dd8cfcef7a9a99b5a7e1e9b45f
luisrodriguez <codekommissar@gmail.com>
2017-11-22 21:24:20 -0400

    make it better

--- bot.js
+++ bot.js
@@ -1,5 +1,55 @@
-var Twit = require("twit");
-var moment = require('moment');
-var config = require("./config");
+const humanizeDuration = require ("humanize-duration");
+const moment           = require ("moment");
+const Twit             =  require ("twit");
+const config           =  require ("./config");
 
-var T = new Twit(config);
+// Sends the first tweet and then the bot keeps sending them every 5 minutes (setInterval)
+sendTweet();
+setInterval(sendTweet, 1000*60*5);
+
+// Connects with the Twitter API and sends our status update
+function sendTweet() {
+    const Tweet = new Twit(config);
+
+    // Arguments to make Tweet.post function work
+    const params = {
+        status: countdownMessage()
+    };
+    function tweet(err, data, response) {
+        if (err) {
+            console.log("Something went wrong :(");
+        } else {
+            console.log("Tweet sent!");
+        }
+    };
+
+    // Sending out tweet
+    Tweet.post('statuses/update', params, tweet);
+};
+
+// Returns the message to send as our status update
+function countdownMessage() {
+    // World Cup Start Date (June 14, 2018)
+    const worldCupDateInfo = {
+        'year': 2018,
+        'month': 5,
+        "date": 14,
+        "hour": 12,
+        "minute": 0,
+        "second": 0
+    };
+
+    // Configuration for making humanizeDuration output a more readable date
+    const humanizeDurationConfig = {
+        largest: 5,
+        round: true,
+        conjunction: ' and ',
+        serialComma: false
+    }
+
+    const now = moment();
+    const worldCupStartDate = moment().set(worldCupDateInfo);
+    const timeDifference = moment.duration(now.diff(worldCupStartDate));
+    const humanReadbleDifference = humanizeDuration(timeDifference, humanizeDurationConfig);
+    return `Only ${humanReadbleDifference} until #Russia2018!`
+};
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// Add your Keys and Access Tokens from https://apps.twitter.com in here
+// Add your personal info for the bot to work in here
 module.exports = {
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...'
+    consumer_key:         '5JbRaa0gHaQHzzDpVCO9wTlWn',
+    consumer_secret:      'ftkEIpM2gD5mBPgDaqkq9GGifTDLh2ZB5PHkwDhjKGDxAhzqzq',
+    access_token:         '933097575636336640-URVT4tyR3nRGhph0BPp2SGR0K9TKCYc',
+    access_token_secret:  'TY8s6RjbqN1oWj85KuNPMCunG6f9x4UPVNeMAg8X9XgtC'
 }
